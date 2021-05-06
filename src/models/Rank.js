329afcb908b1851612dc594f55d87fbf525d3e81
pedroesmerio <p.h.esmerio@gmail.com>
2021-05-06 00:49:28 -0300

    Create Rank/User models (not ready yet)

--- src/models/Rank.js
+++ src/models/Rank.js
@@ -2,18 +2,11 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const modelSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  img: String,
-  level: Number,
-  completed: Number,
-  exp: Number,
   position: Number,
-  passwordHash: String,
   token: String,
 });
 
-const modelName = 'User';
+const modelName = 'Rank';
 
 if (mongoose.connection && mongoose.connection.models[modelName]) {
   module.exports = mongoose.connection.models[modelName];
